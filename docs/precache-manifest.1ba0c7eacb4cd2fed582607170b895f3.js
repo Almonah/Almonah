89544937f7c29656fc361da524ad63ded0b7613e
Nikita Sazonov <almonah@gmail.com>
2021-04-24 23:33:33 +0300

    add docs folder

---
+++ docs/precache-manifest.1ba0c7eacb4cd2fed582607170b895f3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b86a3c1a0afc912ccc1",
+    "url": "/static/css/main.3b363b4e.chunk.css"
+  },
+  {
+    "revision": "0b86a3c1a0afc912ccc1",
+    "url": "/static/js/main.e3217861.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6ab0ba855f5d16a08ed0",
+    "url": "/static/js/2.6617f167.chunk.js"
+  },
+  {
+    "revision": "fffbf2b0615d0c4e3720b7f896e5d60d",
+    "url": "/static/media/hb.fffbf2b0.jpg"
+  },
+  {
+    "revision": "d87eeea14eced0dee549e6590bc07935",
+    "url": "/static/media/question.d87eeea1.jpg"
+  },
+  {
+    "revision": "b60081a0c8f257f18df90b752addfee9",
+    "url": "/static/media/success.b60081a0.jpg"
+  },
+  {
+    "revision": "4dfae95205df67e5cd01bd3c4d157188",
+    "url": "/static/media/gift.4dfae952.png"
+  },
+  {
+    "revision": "99af40ba187d33d3763c05ee3cf77170",
+    "url": "/static/media/1.99af40ba.jpg"
+  },
+  {
+    "revision": "32061ab99fe60ce51208575386c7224f",
+    "url": "/static/media/2.32061ab9.jpg"
+  },
+  {
+    "revision": "e470430857f5bdda77fd6200232a0778",
+    "url": "/static/media/q.e4704308.mp4"
+  },
+  {
+    "revision": "8634567cd1b2445c52188670bfff71e5",
+    "url": "/static/media/a.8634567c.mp4"
+  },
+  {
+    "revision": "c87bacf70e43dcedd88807101ce084b1",
+    "url": "/static/media/reb.c87bacf7.jpg"
+  },
+  {
+    "revision": "cef8981410d59d61ba8fa10f75cf3da7",
+    "url": "/static/media/reb1.cef89814.jpg"
+  },
+  {
+    "revision": "bbfb0e974839d205f490177eeb7778ee",
+    "url": "/index.html"
+  }
+];
